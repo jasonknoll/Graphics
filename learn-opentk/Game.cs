25a893c4228ef1a432bd18924e47a4ea22aa6310
Jason Knoll <jasonknoll98@aol.com>
2024-10-27 15:18:13 -0400

    trying to hello triangle

--- learn-opentk/Game.cs
+++ learn-opentk/Game.cs
@@ -7,6 +7,13 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 public class Game : GameWindow
 {
+    private int VertexBufferObject;
+    float[] triVertices = {
+        -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+        0.5f, -0.5f, 0.0f, //Bottom-right vertex
+        0.0f,  0.5f, 0.0f  //Top vertex
+    };
+    
     public Game(int width, int height, string title) : base(new GameWindowSettings(), new NativeWindowSettings() {
         Size = (width, height), Title = title
     }) {}
@@ -20,5 +27,33 @@ public class Game : GameWindow
             Close();
         }
     }
-    
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        
+        VertexBufferObject = GL.GenBuffer();
+        
+        GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+        
+        GL.BufferData(BufferTarget.ArrayBuffer, triVertices.Length * sizeof(float), triVertices, BufferUsageHint.StaticDraw);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+        
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        
+        SwapBuffers();
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+        
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
 }
